67817d00993831f367fc48b7811aa521bc208604
Cheolho Jeon <cheolho@edu.hanbat.ac.kr>
2021-12-30 18:48:55 +0900

    #3 POST, GET 요청 예제 구현

---
+++ src/main/java/io/charlie/restdocs/HttpResponseBody.java
@@ -0,0 +1,16 @@
+package io.charlie.restdocs;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HttpResponseBody<T> {
+  private String code;
+  private String message;
+  private T data;
+}
